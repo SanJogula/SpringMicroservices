617ca93f46b6115838fd44b6a5de835a169c6090
Santhosh <santhoshkumar.jogula@gmail.com>
2024-06-30 20:16:46 +0530

    Microservices with Spring boot 3

---
+++ service-registry/src/test/java/com/santhosh/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
